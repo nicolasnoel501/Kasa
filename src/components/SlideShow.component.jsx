aac65cecfa993920c761b571ba0b8732f567da05
Nicolas Noël <nicolasnoel501@gmail.com>
2024-03-26 14:13:08 +0100

    add counter to image

--- src/components/SlideShow.component.jsx
+++ src/components/SlideShow.component.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ArrowRight from '../assets/chevron_carousel_right.png';
 import ArrowLeft from '../assets/chevron_carousel_left.png';
 
 const SlideShow = ({ pictures }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [showCount, setShowCount] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowCount(document.body.clientWidth >= 768);
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize(); 
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const handleNextSlide = () => {
     setCurrentSlide((prevSlide) => (prevSlide === pictures.length - 1 ? 0 : prevSlide + 1));
@@ -13,11 +25,9 @@ const SlideShow = ({ pictures }) => {
     setCurrentSlide((prevSlide) => (prevSlide === 0 ? pictures.length - 1 : prevSlide - 1));
   };
 
-  const showArrows = pictures.length > 1;
-
   return (
     <div className="carousel">
-      {showArrows && (
+      {pictures.length > 1 && (
         <div className='carousel_arrow'>
           <img className="carousel_arrow_left" src={ArrowLeft} alt="Previous" onClick={handlePrevSlide} />
           <img className="carousel_arrow_right" src={ArrowRight} alt="Next" onClick={handleNextSlide} />
@@ -33,7 +43,7 @@ const SlideShow = ({ pictures }) => {
         />
       ))}
 
-      {window.innerWidth > 768 && <p className="slideCount">{pictures.length}</p>}
+      {showCount && <p className='slideCount'>{currentSlide + 1} / {pictures.length}</p>}
     </div>
   );
 };
